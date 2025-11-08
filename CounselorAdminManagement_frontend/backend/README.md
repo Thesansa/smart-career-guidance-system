65bbe20b8cf44cd1d2609eea357f156be12ff790
Prarthana <prarthanaalagoda@gmail.com>
2025-11-09 02:53:32 +0530

    Add Counselor Admin Management frontend by Prarthana

--- CounselorAdminManagement_frontend/backend/README.md
+++
@@ -1,2 +0,0 @@
-Backend (Spring Boot) — instructions here
-17.10.2025 - build up the authentication backend
--- CounselorAdminManagement_frontend/backend/career-guidance-backend/src/main/java/com/smartcareer/careerguidancebackend/repository/RoleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.smartcareer.careerguidancebackend.repository;
-import com.smartcareer.careerguidancebackend.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
